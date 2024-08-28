6e1053ef1d03e02c6e42615c3807b153fc5c94b4
golam71 <74001687+golam71@users.noreply.github.com>
2024-08-28 22:16:10 +0600

    ✅ Add reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let charArray = string.split("");
+  let revArray = []; //use at for loop later
+  let length = charArray.length;
+  for (let i = 0; i <= length; i++) {
+    revArray.push(charArray[length - i]);
+    // get the max number of chars and - em to get the i from backwards
+  }
+  let reversedString = revArray.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
