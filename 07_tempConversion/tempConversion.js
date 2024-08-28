7bfb0aeac6debed92fcccd8e3c968e0066a567aa
golam71 <74001687+golam71@users.noreply.github.com>
2024-08-28 23:06:39 +0600

    ⚡️ temp conversation

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  let celcius = ((fahr - 32) / 9) * 5;
+  return Math.floor(celcius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celc) {
+  let fahrenheit = (celc / 5) * 9 - 32;
+  return Math.floor(fahrenheit);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
