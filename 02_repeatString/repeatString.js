e7141aa8df069bc67baca6696d0dbcf84b1af533
golam71 <74001687+golam71@users.noreply.github.com>
2024-08-29 15:19:43 +0600

    🐛 Fix test cases to not skip

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function (string, repeatNumber) {
   let newString = "";
+  if (repeatNumber < 0) {
+    return "ERROR";
+  }
   for (let i = 0; i < repeatNumber; ++i) {
     newString += string;
   }
