1807b265a3396729db09bc9f10c5ee2506280298
golam71 <74001687+golam71@users.noreply.github.com>
2024-08-30 22:23:40 +0600

    🐛 Critical bug fix on for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,27 @@
-const sumAll = function (firstNumber, lastNumber) {
-  let sum = 0;
-  for (let i = firstNumber; i <= lastNumber; i++) {
-    sum += i;
+const sumAll = function (number1, number2) {
+  if (
+    number1 < 0 ||
+    number2 < 0 ||
+    !Number.isInteger(number1) ||
+    !Number.isInteger(number2) 
+  ) {
+    return "ERROR";
   }
-  return sum;       
+  let firstNumber, lastNumber;
+  let sum = 0;
+  if (number1 > number2) {
+    lastNumber = number1;
+    firstNumber = number2;
+  }
+  if (number1 < number2) {
+    lastNumber = number2;
+    firstNumber = number1;
+  }
+
+  for (let i = firstNumber; i <= lastNumber; i++) {
+    sum = sum + i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
