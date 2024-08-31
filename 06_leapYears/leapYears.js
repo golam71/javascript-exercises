4acace2d8a84d745c5620f6fce051bbda1c5936c
golam71 <74001687+golam71@users.noreply.github.com>
2024-08-31 20:17:59 +0600

    🚑 fix on leapyears and fix tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,13 @@
-const leapYears = function(year) {
-    if (year %4 === 0){
-        return true;
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 !== 0) {
+      return true;
     }
-    return false;
+    if (year % 100 === 0 && year % 400 === 0) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Do not edit below this line
