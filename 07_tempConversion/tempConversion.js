d9da93fa4bf905c509300e89497ca47631e99caf
golam71 <74001687+golam71@users.noreply.github.com>
2024-08-31 20:45:09 +0600

    :tada: fix for temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function (fahr) {
   let celcius = ((fahr - 32) / 9) * 5;
-  return Math.floor(celcius);
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function (celc) {
-  let fahrenheit = (celc / 5) * 9 - 32;
-  return Math.floor(fahrenheit);
+const convertToFahrenheit = function (calc) {
+  let fahrenheit = ((calc / 5) * 9 )+ 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
