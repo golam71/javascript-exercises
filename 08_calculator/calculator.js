5afe84124a5f0269051e2b88d27d1f8c6dff11b1
golam71 <74001687+golam71@users.noreply.github.com>
2024-09-01 17:13:09 +0600

    :bento: fix calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,25 +6,38 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function (...numbers) {
-  let numberArray = [...numbers];
-  let sumOfNumbers = 0;
-  numberArray.map((item) => {
-    sumOfNumbers += item;
-  });
-  return sumOfNumbers;
+const sum = function (data) {
+  let sumOfArray = 0;
+  for (let i = 0; i < data.length; i++) {
+    sumOfArray += Number(data[i]);
+  }
+  return sumOfArray;
 };
 
-const multiply = function (a, b) {
-  return a * b;
+const multiply = function (data) {
+  let multipleOfArray = 1;
+  for (let i = 0; i < data.length; i++) {
+    multipleOfArray *= Number(data[i]);
+  }
+  return multipleOfArray;
 };
 
 const power = function (a, b) {
-  return a ** b;
+  if (b !== 0) {
+    return a ** b;
+  }
+  return 0;
 };
 
-const factorial = function (a, b) {
-  return Math.floor(a ** -b);
+const factorial = function (a) {
+  let factorialAnswer = 1;
+  if (a !== 0) {
+    for (let i =1;i<a;i++){
+      factorialAnswer *= i;
+    }
+    return factorialAnswer;
+  }
+  return 1;
 };
 
 // Do not edit below this line
