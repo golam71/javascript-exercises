789817d981d2d320a38e778dc5e2be3664992aec
golam71 <74001687+golam71@users.noreply.github.com>
2024-09-01 18:57:07 +0600

    :bento: fix palindomes detector

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = (string) => {
+    // Filter out non-alphanumeric characters and convert to lower case
+    let cleanString = string
+        .toLowerCase()
+        .split("")
+        .filter(char => "abcdefghijklmnopqrstuvwxyz0123456789".includes(char))
+        .join("");
 
-};
+    // Reverse the cleaned string
+    let revString = cleanString.split("").reverse().join("");
+
+    // Check if the cleaned string is equal to its reversed version
+    return cleanString === revString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
