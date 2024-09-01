d1c91a9b8a803eea2fd72b42061c99a7b839bbed
golam71 <74001687+golam71@users.noreply.github.com>
2024-09-02 00:50:57 +0600

    🎉 End test fixes

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let i = 0;
+  let age;
+  let peopleArray = [];
 
+  for (i = 0; i < people.length; i++) {
+    age = people[i].yearOfDeath - people[i].yearOfBirth;
+    console.log(age);
+    if (typeof people[i].yearOfDeath === "undefined") {
+      console.log("age is not number");
+      age = new Date().getFullYear() - people[i].yearOfBirth;
+    }
+    peopleArray.push(age);
+  }
+  let sortedArray = peopleArray.toSorted(function (a, b) {
+    return a - b;
+  });
+  let greatestAge = sortedArray[sortedArray.length - 1];
+  let personIndex = peopleArray.indexOf(greatestAge);
+  return people[personIndex];
 };
 
 // Do not edit below this line
