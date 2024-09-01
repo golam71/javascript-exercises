2c4a6d17cb85d7ab442a05602260adab308e54c8
golam71 <74001687+golam71@users.noreply.github.com>
2024-09-01 23:14:38 +0600

    :bento: fix fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    if (n == 0){
+        return 0
+    }
+
+    if(n < 0){
+        return "OOPS"
+    }
+    if(n == 1){
+        return 1;
+    }
+
+    if(n == 2){
+        return 1;
+    }
+
+    n = Number(n)
+
+    let number1 = 1;
+    let number2 = 1;
+    let lastNumber=0;
+    for (let i =2 ; i < n ; i++){
+        lastNumber = number1+number2;
+        number1 = number2;
+        number2 = lastNumber;
+    }
+    return lastNumber;
 
 };
 
