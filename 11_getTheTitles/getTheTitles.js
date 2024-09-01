22f7b8dbf91aae0e82f1e2d56ab815a290e73ffc
golam71 <74001687+golam71@users.noreply.github.com>
2024-09-01 23:22:25 +0600

    :bento: fix books titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let booksTitles = [];
+    for (let i = 0; i < books.length; i++) {
+        booksTitles.push(books[i].title)
+    }
+    return booksTitles;
 };
 
 // Do not edit below this line
